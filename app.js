5bf8a33baf6c08ec8da004d8bc9008f1c10de5c8
Christophe Lemercier <lemercier60@gmail.com>
2020-04-11 19:40:41 +0200

    Empeche les injection noSQL

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
+const mongoSanitize = require('express-mongo-sanitize');
 
 dotenv.config({ path: './config/config.env' });
 
@@ -20,6 +21,8 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+app.use(mongoSanitize());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
