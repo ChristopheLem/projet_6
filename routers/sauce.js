38c76bf288ce23bb46ce72ec587a99882ac2b97f
Christophe Lemercier <lemercier60@gmail.com>
2020-04-11 13:13:14 +0200

    Ajoute router sauce.js

---
+++ routers/sauce.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const sauceCtrller = require('../controllers/sauce');
+
+router.get('/', auth, sauceCtrller.getSauces);
+router.get('/:id', auth, sauceCtrller.getSauce);
+router.post('/', auth, multer, sauceCtrller.createSauce);
+router.put('/:id', auth, multer, sauceCtrller.updateSauce);
+router.delete('/:id', auth, multer, sauceCtrller.deleteSauce);
+router.post('/:id/like', auth, sauceCtrller.likeOrDislike);
+
+
+module.exports = router;
